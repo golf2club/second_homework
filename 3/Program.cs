0b1611ac8455cb33b7a0dca3fb3d6f0830925a8f
egor <akhramovich.e.i@gmail.com>
2022-12-20 00:13:26 +0300

    hw

---
+++ 3/Program.cs
@@ -0,0 +1,8 @@
+﻿number = IBinaryInteger ( input("Введите число дня недели от 1 до 7: "));
+
+if (number < 1) or (number > 7) :
+    print('Неверное число)')
+else number > 5:
+    print('Выходной!')
+else:
+    print('Рабочий день')
